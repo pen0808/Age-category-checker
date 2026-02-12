6c5a5cb086dc3b3d3fd040cc7f3dd52397b31bed
Paul Nwaoduah <nwaoduahpaul@gmail.com>
2026-02-12 14:19:49 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let age = 100;
+let message = "";
+
+if(age < 0){
+  message = "Please enter a number above 0";
+  console.log(message);
+}else if(age <= 17){
+  message = "You are a teenager";
+  console.log(message);
+}else if(age <= 49){
+  message = "You are an adult";
+  console.log(message);
+}else if(age <= 60){
+  message = "You are a senior adult";
+  console.log(message);
+}else{
+  message = "You are a senior citizen";
+  console.log(message);
+}
